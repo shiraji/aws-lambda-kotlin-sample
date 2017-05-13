211826764a62c03915281db044712a89cd2609d1
shiraji <isogai.shiraji@gmail.com>
2017-05-14 08:58:17 +0900

    Use RequestHandler for sending location JSON

--- src/main/java/com/github/shiraji/foo/FooHanlder.kt
+++ src/main/java/com/github/shiraji/foo/FooHanlder.kt
@@ -1,12 +1,16 @@
 package com.github.shiraji.foo
 
 import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
 
-class FooHanlder {
-
-    fun handler(count: Map<String, Object>, context: Context): String {
-        val logger = context.logger
-        logger.log("fooooooo: $count --- context: $context")
-        return "context: $context"
+class FooHanlder : RequestHandler<FooHanlder.Request, FooHanlder.Response> {
+    override fun handleRequest(input: Request?, context: Context?): Response {
+        val logger = context?.logger ?: throw IllegalArgumentException()
+        logger.log("input: $input")
+        return Response(location = "https://github.com/shiraji/aws-lambda-kotlin-sample")
     }
+
+    class Request
+
+    data class Response(val location: String)
 }
